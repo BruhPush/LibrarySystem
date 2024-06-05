c9237ce9a0c3b8cad1bf9906e12c5dfab2d5c360
BruhPush <153186690+BruhPush@users.noreply.github.com>
2024-06-05 22:54:00 +0800

    first commit

---
+++ program/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <el-container>
+        <el-header style="background-color: #333333;height:125px">
+            <img src="@/assets/logo.svg" alt="" style="height:100px;position:relative;top:10px">
+            <span style="font-size:20px;margin-left:15px;color:white">湘潭大学电子图书馆</span>
+        </el-header>
+    </el-container>
+    <el-container>
+        <el-aside style="overflow:hidden;min-height:100vh;background-color:#535c64;width:250px">
+
+        </el-aside>
+        <el-main>
+
+        </el-main>
+    </el-container>
+  </div>
+</template>
---
+++ program/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <el-container>
+        <el-header style="background-color: #4c535a;height:200px">
+
+        </el-header>
+    </el-container>
+
+    <el-container>
+        <el-aside style="overflow: hidden; min-height: 100vh; backgound-color: #545c64; width:250px">
+
+        </el-aside>
+        <el-main>
+
+        </el-main>
+    </el-container>
+  </div>
+</template>
+
+
